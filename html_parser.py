cdf3d6082a7f74f353b72f36a47114cf8fe39f48
hexiaodongRUC <hexiaodong_1993@163.com>
2024-03-19 22:40:34 +0800

    add html parser

--- html_parser.py
+++ html_parser.py
@@ -1,22 +1,27 @@
 # html_parser.py
 
 from bs4 import BeautifulSoup
+import re
 
 def parse_html(html_dict):
     ticket_prices_dict = {}
     # Parse the HTML using BeautifulSoup or any other HTML parsing library
     for idx, html in html_dict.items():
         soup = BeautifulSoup(html, 'html.parser')
+        recommendations = soup.find_all("div", class_="BpkTicket_bpk-ticket__NzNiO")
+        print(idx)
         # 找到推荐的机票信息所在的容器
-        recommendations = soup.find_all("div", class_="BpkText_bpk-text__2NH4L BpkText_bpk-text--body__19Pnr DetailsPanelContainer_priceDetails__3gpkw")
-
-        # 提取前5行推荐的机票价格和航空公司
-        for index, recommendation in enumerate(recommendations[:5], start=1):
-            price = recommendation.find("span", class_="BpkText_bpk-text__2NH4L BpkText_bpk-text--lg__2gZYD")
-            airline = recommendation.find("span", class_="BpkText_bpk-text__2NH4L BpkText_bpk-text--sm__33PGB DetailsPanelContainer_airline__1FgJH")
-            
-            if price and airline:
-                print(f"Recommendation {index}: Price - {price.text.strip()}, Airline - {airline.text.strip()}")
+        for recommendation in recommendations:
+            try:
+                aircompany = recommendation.find("span", class_="BpkText_bpk-text__MWZkY BpkText_bpk-text--xs__ZDJmY").get_text()
+                price = recommendation.find("span", class_="BpkText_bpk-text__MWZkY BpkText_bpk-text--lg__NjNhN").get_text()
+                fly_way = recommendation.find("span", class_=re.compile("^BpkText_bpk-text__MWZkY BpkText_bpk-text--xs__ZDJmY LegInfo_stopsLabel")).get_text()
+                time_duration=recommendation.find("span", class_="BpkText_bpk-text__MWZkY BpkText_bpk-text--xs__ZDJmY Duration_duration__NmUyM").get_text()
+                dep_time = recommendation.find_all("span", class_="BpkText_bpk-text__MWZkY BpkText_bpk-text--subheading__NzkwO")[0].get_text()
+                des_time = recommendation.find_all("span", class_="BpkText_bpk-text__MWZkY BpkText_bpk-text--subheading__NzkwO")[1].get_text()
+                print(f"{aircompany}, {price}, {fly_way}, {time_duration}, {dep_time}, {des_time}")
+            except:
+                print(Exception)
             
 
     # Extract relevant ticket price information
