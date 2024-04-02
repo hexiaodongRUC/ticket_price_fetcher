5be5803a55da4a20a96d61e0d9f416892b39983d
hexiaodongRUC <hexiaodong_1993@163.com>
2024-04-02 23:14:09 +0800

    支持去程和返程同时记录

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 DEPARTURE=["bjsa"]
-#DESTINATION=["it", "FR", "CH", "ES", "HR", "at"]    #country id
-#DESTINATION=["rome", "mila", "veni", "pari", "barc", "madr", "zuri", "gene", "zagr", "dubr", "vien", "bud"]
-DESTINATION=["veni", "pari", "barc", "madr", "zuri", "gene", "zagr", "dubr", "vien", "bud"]
+DESTINATION=["rome", "mila", "veni", "pari", "barc", "madr", "zuri", "gene", "zagr", "dubr", "vien", "bud"]
 DEPARTURE_TIME = "20241001"
 RETURN_TIME = "20241014"
--- http_requests.py
+++ http_requests.py
@@ -18,6 +18,7 @@ def get_html_from_skyscanner():
     
 
     html_dict = {}    #save the htmls
+
     for dep in config.DEPARTURE:
        for des in config.DESTINATION:        
             driver = webdriver.Chrome(options=options)  # You might need to specify the path to your Chrome driver executable  
@@ -29,6 +30,19 @@ def get_html_from_skyscanner():
             html_dict[f'{dep}-{des}'] = html
             driver.quit()  # Close the browser
             time.sleep(30)
+
+    for dep in config.DEPARTURE:
+       for des in config.DESTINATION:        
+            driver = webdriver.Chrome(options=options)  # You might need to specify the path to your Chrome driver executable  
+            price_list_html = f"https://www.skyscanner.com/transport/flights/{des}/{dep}/{config.RETURN_TIME}/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false"
+            driver.get(price_list_html)
+            # Wait for the JavaScript content to fully load
+            WebDriverWait(driver, 30)
+            html = driver.page_source
+            html_dict[f'{des}-{dep}'] = html
+            driver.quit()  # Close the browser
+            time.sleep(30)
+    
     return html_dict
 
 if __name__  == '__main__':
